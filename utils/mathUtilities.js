aa5d2bee74f15872e43eae282c5d5a9cad758908
StevenScript <sdn709466@gmail.com>
2024-10-15 18:46:26 -0230

    Built getQuestion
    built function that generates random math questions

--- utils/mathUtilities.js
+++ utils/mathUtilities.js
@@ -3,7 +3,48 @@
  *
  * @returns {} The randomly generated math question
  */
-function getQuestion() {}
+function getQuestion() {
+  // Generate two random numbers between 1 and 10
+  const num1 = Math.floor(Math.random() * 10) + 1;
+  const num2 = Math.floor(Math.random() * 10) + 1;
+
+  // Define possible operations
+  const operations = ["+", "-", "*", "/"];
+
+  // Randomly select an operation
+  const operation = operations[Math.floor(Math.random() * operations.length)];
+
+  let questionText; // Will hold the question as a string
+  let answer; // Will hold the correct answer as a number
+
+  // Determine the question and answer based on the operation
+  switch (operation) {
+    case "+":
+      questionText = `${num1} + ${num2}`;
+      answer = num1 + num2;
+      break;
+    case "-":
+      questionText = `${num1} - ${num2}`;
+      answer = num1 - num2;
+      break;
+    case "*":
+      questionText = `${num1} * ${num2}`;
+      answer = num1 * num2;
+      break;
+    case "/":
+      // For division, ensure result is a whole number
+      const product = num1 * num2; // Multiply to get a clean dividend
+      questionText = `${product} / ${num1}`; // So answer will be num2
+      answer = product / num1;
+      break;
+  }
+
+  // Return the question and answer as an object
+  return {
+    questionText,
+    answer,
+  };
+}
 
 /**
  * Parses the provided question and gets whether or not the provided answer is correct
